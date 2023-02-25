7e70b78b482c61ac97052d9dfcd26a3ac850ef9f
jonah lozano <jonahlozanobusiness@gmail.com>
2023-02-25 10:20:42 -0800

    route/logic boilerplate

--- DisLi/src/routes/help.ts
+++ DisLi/src/routes/help.ts
@@ -1,13 +1,43 @@
 import express from "express";
 const router = express.Router();
+import { Help } from "../entity/help";
+import bodyParser from "body-parser";
+import { EntityNotFoundError, QueryFailedError } from "typeorm";
+var jsonParser = bodyParser.json();
 
 // ADMIN
 // view help request queue
-router.get("/", async (req, res) => {
-  req.params;
-  res.status(200).send("get-help-all");
+router.get("/", async (_req, res) => {
+  try {
+    const item_details = await Help.findBy({});
+
+    res.status(200).json(item_details);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(404).send("ERROR");
+  }
 });
 
+// ADMIN/(TEACHER IF AUTHORIZED)
+// view specific help request details
+router.get("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const item_details = await Help.findOneByOrFail({ id });
+
+    res.status(200).json(item_details);
+  } catch (err) {
+    console.log(err.stack);
+    if (err instanceof EntityNotFoundError) {
+      res.status(404).send(err.message);
+    } else {
+      res.status(400).send("ERROR");
+    }
+  }
+});
+
+// TODO
 // ADMIN/TEACHER
 // view your help requests
 router.get("/user", async (req, res) => {
@@ -15,24 +45,87 @@ router.get("/user", async (req, res) => {
   res.status(200).send("get-help-mine");
 });
 
-// ADMIN// ADMIN
-// delete inventory
-router.delete("/", async (req, res) => {
-  req.params;
-  res.status(200).send("delete-help");
-});
-// view specific help request details
-router.get("/:id", async (req, res) => {
-  req.params;
-  res.status(200).send("get-help-specific");
+// ADMIN/TEACHER
+// add help requests
+router.post("/", jsonParser, async (req, res) => {
+  try {
+    const first_name = req.body.first_name;
+    const last_name = req.body.last_name;
+    const room = req.body.room;
+    const problem = req.body.problem;
+    const resolved = req.body.resolved;
+
+    // TODO: REMEMBER TO ADD UNIVERSITY_ID with PERSON enitity
+
+    const new_device = Help.create({
+      first_name,
+      last_name,
+      room,
+      problem,
+      resolved,
+    });
+
+    await Help.insert(new_device);
+
+    res.status(201).json(new_device);
+  } catch (err) {
+    console.log(err.stack);
+    if (err instanceof QueryFailedError) {
+      res.status(404).send(err.message);
+    } else {
+      res.status(400).send("ERROR");
+    }
+  }
 });
 
+// ADMIN/TEACHER
+// delete inventory
+router.delete("/", jsonParser, async (req, res) => {
+  try {
+    const id = req.body.id;
+
+    const existing_item = await Help.findOneByOrFail({ id });
+
+    await Help.remove(existing_item);
+
+    res.status(204).send();
+  } catch (err) {
+    console.log(err.stack);
+    if (err instanceof EntityNotFoundError) {
+      res.status(404).send(err.message);
+    } else {
+      res.status(400).send("ERROR");
+    }
+  }
+});
 
 // ADMIN/TEACHER
 // change resolved for specific application
-router.put("/", async (req, res) => {
-  req.params;
-  res.status(200).send("put-help");
+router.put("/", jsonParser, async (req, res) => {
+  try {
+    const id = req.body.id;
+
+    const existing_item = await Help.findOneByOrFail({ id });
+
+    existing_item.first_name = req.body.first_name;
+    existing_item.last_name = req.body.last_name;
+    existing_item.room = req.body.room;
+    existing_item.problem = req.body.problem;
+    existing_item.resolved = req.body.resolved;
+
+    await Help.update({ id }, existing_item);
+
+    res.status(201).json(existing_item);
+  } catch (err) {
+    console.log(err.stack);
+    if (err instanceof EntityNotFoundError) {
+      res.status(404).send(err.message);
+    } else if (err instanceof QueryFailedError) {
+      res.status(400).send(err.message);
+    } else {
+      res.status(400).send("ERROR");
+    }
+  }
 });
 
 export = router;
--- DisLi/src/routes/program.ts
+++ DisLi/src/routes/program.ts
@@ -1,26 +1,53 @@
 import express from "express";
 const router = express.Router();
+import { Program } from "../entity/program";
+import bodyParser from "body-parser";
+import { QueryFailedError } from "typeorm";
+var jsonParser = bodyParser.json();
 
 // ADMIN
 // view programs
-router.get("/", async (req, res) => {
-  req.params;
-  res.status(200).send("get-program");
+router.get("/", async (_req, res) => {
+  try {
+    const item_details = await Program.findBy({});
+
+    res.status(200).json(item_details);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(404).send("ERROR");
+  }
 });
 
 // ADMIN
 // add programs
-router.post("/", async (req, res) => {
-    req.params;
-    res.status(200).send("post-inventory");
-  });
+router.post("/", jsonParser, async (req, res) => {
+  try {
+    const code_name = req.body.code_name;
+    const availiable_to = req.body.availiable_to;
+
+    const new_device = Program.create({
+      code_name,
+      availiable_to,
+    });
+
+    await Program.insert(new_device);
+
+    res.status(201).json(new_device);
+  } catch (err) {
+    console.log(err.stack);
+    if (err instanceof QueryFailedError) {
+      res.status(404).send(err.message);
+    } else {
+      res.status(400).send("ERROR");
+    }
+  }
+});
 
 // ADMIN
 // modify/depricate inventory
 router.put("/", async (req, res) => {
-    req.params;
-    res.status(200).send("put-inventory");
-  });
-
+  req.params;
+  res.status(200).send("put-inventory");
+});
 
 export = router;
