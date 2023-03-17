53c4904866e3a91ed1fd94839be9fbafbb844343
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2023-03-16 18:23:24 -0700

    testing inventory_logic with modo

--- DisLi/src/logic/inventory_logic.ts
+++ DisLi/src/logic/inventory_logic.ts
@@ -17,9 +17,70 @@ const view_item = async (req: Request, res: Response) => {
   try {
     const serial_number = req.params.id;
 
+    const data = {
+      metadata: {
+        version: "2.0",
+      },
+      contentContainerWidth: "narrow",
+      content: [
+        {
+          elementType: "blockHeading",
+          heading: "Inventory",
+          headingLevel: 1,
+          headingFontWeight: "light",
+          description:
+            "<strong>Filter:</strong> <br></br>code name: 'Freshman iPads'",
+          descriptionLineHeight: "loose",
+          descriptionFontStyle: "italic",
+          descriptionTextColor: "#566573",
+        },
+        {
+          elementType: "container",
+          id: "custom_styling",
+          content: [
+            {
+              elementType: "divider",
+            },
+            {
+              elementType: "blockHeading",
+              heading: "1941ce97-f86c-4b43-813e-1661f5614e1b",
+              headingLevel: 2,
+              description:
+                "<span style='color:red;font-size:1.0025rem'>Checked Out</span><span style='font-size:1.0025rem'> - Apple iPad (2022)</span><br></br>Freshman iPads",
+              buttons: [
+                {
+                  elementType: "linkButton",
+                  title: "information",
+                  icon: "notification_information",
+                  iconPosition: "iconOnly",
+                  actionStyle: "normal",
+                  link: {
+                    relativePath: " ",
+                  },
+                },
+                {
+                  elementType: "linkButton",
+                  title: "delete",
+                  icon: "delete",
+                  actionStyle: "destructive",
+                  iconPosition: "iconOnly",
+                  confirmationMessage: "Are you sure you want to delete this?",
+                  link: {
+                    relativePath: " ",
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+
     const item_details = await Item.findOneByOrFail({ serial_number });
 
-    res.status(200).json(item_details);
+    console.log(item_details);
+
+    res.status(200).json(data);
   } catch (err) {
     console.log(err.stack);
     if (err instanceof EntityNotFoundError) {
