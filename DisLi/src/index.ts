75f685213ee02ab533fb1b6bc6eea5e65bcdfa6f
JonahLozano <111929937+JonahLozano@users.noreply.github.com>
2023-03-17 15:56:27 -0700

    removed test route

--- DisLi/src/index.ts
+++ DisLi/src/index.ts
@@ -16,60 +16,6 @@ app.get("/", (_, res) => {
   res.send("SUP");
 });
 
-app.get("/test", (req, res) => {
-  // console.log(req.rawHeaders);
-  const tmp = req.rawHeaders.findIndex((ele) => ele === "Authorization");
-  let jsToken: string;
-  if (tmp) {
-    jsToken = req.rawHeaders[tmp + 1].split(" ")[1];
-    console.log(jsToken);
-  }
-
-  const thing = {
-    metadata: {
-      version: "2.0",
-    },
-    contentContainerWidth: "narrow",
-    content: [
-      {
-        elementType: "divider",
-        borderColor: "transparent",
-      },
-      {
-        elementType: "toolbar",
-        id: "label_menu",
-        left: [
-          {
-            elementType: "toolbarMenu",
-            showDisclosureIcon: true,
-            items: [
-              {
-                title: "Menu Category A",
-              },
-              {
-                title: "Menu Category B",
-              },
-              {
-                title: "Menu Category C",
-              },
-              {
-                title: "Menu Category D",
-              },
-            ],
-          },
-        ],
-        middle: [
-          {
-            elementType: "toolbarLabel",
-            label: "Toolbar Label",
-          },
-        ],
-      },
-    ],
-  };
-  res.status(200).send(thing);
-});
-
 app.get("/dev/wipe-database", async (_, res) => {
   clearDB();
   res.send("deleted");
